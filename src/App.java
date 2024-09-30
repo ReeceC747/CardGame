581b2cd3a13823231a9963bc968ae972f0a38119
ReeceC747 <Reececohen747@gmail.com>
2024-09-30 18:33:42 -0400

    first commit

---
+++ src/App.java
@@ -0,0 +1,52 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        ((
+        
+        )System.out.println("Hello, World!");
+            }
+            /**
+             * Rules
+             * both players have a 60 card deck
+             * No more than 4 of a card except it is an energy
+             * THere are 3 types of cards, pokemon cards, Trainer cards, and energy cards
+             * 
+             * ~~~~~~~~~~~~~~~~~~~~
+             * 
+             * You can at the start of the game both players draw 7 cards
+             * 
+             * If there is no pokemon in your hand you have to redraw a new hand and the opponent gets to draw a card
+             * 
+             * After this, both players take 6 cards and sets them aside. These cards are prizes
+             * 
+             * Each time you knock out a pokemon, you get one of your prizes to your hand)
+             * 
+             * you win the game when you collect all 6 prizes. (there are other ways)
+             * 
+             * Each player takes a turn
+             * They can draw a card, play a trainer cards, play a pokemon card, play an energy card, or attack
+             * 
+             * To attack your pokemon must have the energy required to attack. It deals damage to oppenents HP
+             * 
+             * There are 6 spots on each persons field. one is the active spot, 5 are the bench spot, at most you can only have
+             * 6 pokemon on in play byt only the one in the active spot can attack
+             * 
+             * Make a card class
+             * 
+             * Write a loop to fill an array with 60 cards
+             * 
+             * write a loop that can draw 7 cards from the deck
+             * 
+             * write an evaluator that determines if there are any pokemon in the hand
+             * 
+             * ~~~~~~~~~~~
+             * 
+             * this is a monte carlo experiment
+             * 
+             * what if the deck has only 1 pokemon card
+             * 
+             * how many shuffles on average does it take to get the pokemon card
+             * 
+             * What if the deck has 2, 3, ...
+             * write an engine to generate 1 - 60 pokeomn output the odds of having a pokemon in your opening hand. Graph in excel
+     */
+}
